83193ad90b7b611f4ceeebbdb88158894fa169ee
yogi456 <yogi456>
2020-06-27 07:11:10 +0530

    ifelse program

---
+++ ifelse.py
@@ -0,0 +1,9 @@
+#WAP to find number is even or odd
+
+a =input("Please type any number")
+a= int(a)
+if(a%2==0):
+    print(a,"is even")
+else:
+    print("Number is odd")
+print("All work Done !!!!")
